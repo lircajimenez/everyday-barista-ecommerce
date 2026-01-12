a762b73b6e374a049e44e5fc5249f742566acf60
Lirca Jiménez <78028056+lircajimenez@users.noreply.github.com>
2026-01-12 10:58:54 -0500

    better UI in success page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useCartStore } from "@/store/cart-store";
@@ -13,52 +14,64 @@ function CheckoutPage() {
   if (total === 0 || items.length === 0) {
     return (
       <div>
-        <h1>Your Cart is Empty.</h1>
+        <p>Your Cart is Empty.</p>
       </div>
     );
   }
+
+  console.log(items);
 
   return (
     <>
       <Breadcrumb customTitle="Checkout" />
       <div className="container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-8 text-center">Checkout</h1>
-        <Card className="max-w-md mx-auto mb-8">
+        <Card className="max-w-xl mx-auto mb-8">
           <CardHeader>
-            <CardTitle className="text-xl font-bold">Order Summary</CardTitle>
+            <CardTitle className="text-xl text-center font-bold">Order Summary</CardTitle>
           </CardHeader>
           <CardContent>
             <ul className="space-y-4">
               {items.map((item) => (
-                <li className="flex flex-col gap-2 border-b pb-2" key={item.id}>
-                  <div className="flex justify-between">
-                    <span className="font-medium">{item.name}</span>
-                    <span className="font-semibold">${((item.price * item.quantity) / 100).toFixed(2)}</span>
-                  </div>
-                  <div className="flex items-center gap-2">
-                    <Button variant="outline" size="sm" onClick={() => removeItem(item.id)}>
-                      -
-                    </Button>
-                    <span className="text-lg font-semibold">{item.quantity}</span>
-                    <Button size="sm" onClick={() => addItem({ ...item, quantity: 1 })}>
-                      +
-                    </Button>
+                <li className="flex flex-col gap-2 border-b pb-2 last:border-b-0" key={item.id}>
+                  <div className="flex items-center justify-between h-38">
+                    <div className="relative h-full w-full md:w-1/2 rounded-lg overflow-hidden basis-[25%]">
+                      <Image
+                        className="transition duration-300 hover:opacity-90"
+                        fill={true}
+                        objectFit="contain"
+                        alt={item.name}
+                        src={item.imageUrl || ""}
+                      />
+                    </div>
+                    <div className="flex flex-col justify-evenly h-full basis-[50%]">
+                      <span className="font-medium text-center">{item.name}</span>
+                      <div className="flex items-center justify-center gap-2">
+                        <Button className="cursor-pointer" variant="outline" size="sm" onClick={() => removeItem(item.id)}>
+                          -
+                        </Button>
+                        <span className="text-lg font-semibold ">{item.quantity}</span>
+                        <Button className="cursor-pointer" size="sm" onClick={() => addItem({ ...item, quantity: 1 })}>
+                          +
+                        </Button>
+                      </div>
+                    </div>
+                    <span className="font-semibold text-right basis-[15%]">${((item.price * item.quantity) / 100).toFixed(2)}</span>
                   </div>
                 </li>
               ))}
             </ul>
 
-            <div className="mt-4 border-t pt-2 text-lg font-semibold">Total:${(total / 100).toFixed(2)}</div>
+            <div className="mt-4 border-t pt-2 text-lg text-right font-semibold">Total: ${(total / 100).toFixed(2)}</div>
           </CardContent>
         </Card>
 
         <form className="max-w-md mx-auto" action={checkoutAction}>
           <input type="hidden" name="items" value={JSON.stringify(items)} />
-          <Button className="w-full" type="submit" variant="default">
+          <Button className="w-full mb-5 cursor-pointer" type="submit" variant="default">
             Proceed to Payment
           </Button>
 
-          <Button className="w-full" variant="default" onClick={() => clearCart()}>
+          <Button className="w-full cursor-pointer" variant="outline" onClick={() => clearCart()}>
             Clear Cart
           </Button>
         </form>
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import Link from "next/link";
 import { useCartStore } from "@/store/cart-store";
 import Breadcrumb from "@/components/breadcrumb";
+import { CheckBadgeIcon, CheckCircleIcon } from "@heroicons/react/24/outline";
 
 function SuccessPage() {
   const { clearCart } = useCartStore();
@@ -13,7 +14,8 @@ function SuccessPage() {
   return (
     <div className="container mx-auto px-4 py-8 text-center">
       <Breadcrumb customTitle="Order Success" />
-      <h1 className="text-3xl font-bold mb-4">Payment Successful!</h1>
+      <CheckCircleIcon className="h-40 w-40 text-green-500 m-auto" />
+      <h2 className="text-3xl font-bold mb-4">Payment Successful!</h2>
       <p className="mb-4">Thank you for your purchase. Your order is being processed.</p>
       <Link href="/products" className="text-blue-600 hover:underline">
         Continue Shopping
